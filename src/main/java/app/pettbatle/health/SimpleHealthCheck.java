1bfdf091d32b79724f252769b36fcf7f87885918
Mike Hepburn <eformat@gmail.com>
2020-03-23 12:39:45 +1000

    🦄 add simple health readiness check 🦄

---
+++ src/main/java/app/pettbatle/health/SimpleHealthCheck.java
@@ -0,0 +1,16 @@
+package app.pettbatle.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+@Readiness
+public class SimpleHealthCheck implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Simple health check").up().build();
+    }
+}
