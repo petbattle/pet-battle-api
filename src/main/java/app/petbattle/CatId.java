7822e1f2940b0d4c7fdbbcd4beb57c4e0b27122b
Mike Hepburn <eformat@gmail.com>
2020-07-22 11:56:10 +1000

    🐀 fix cat ids 🐀

--- src/main/java/app/petbattle/CatId.java
+++ src/main/java/app/petbattle/CatId.java
@@ -1,9 +1,11 @@
 package app.petbattle;
 
 import io.quarkus.mongodb.panache.ProjectionFor;
+import org.bson.types.ObjectId;
 
 @ProjectionFor(Cat.class)
 public class CatId {
+    public ObjectId id;
     public Integer count;
     public Boolean vote;
 }
